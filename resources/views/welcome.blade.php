bb8ea6c9d6a60a999701d42f6cd36b55f42e0233
unknown <guilhermedeoliveiradesouza@gmail.com>
2023-07-27 11:09:26 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="/css/styles.css" rel="stylesheet" />
+        <script src="/js/scripts.js"></script>
+
+    </head>
+    <body class="antialiased">
+        <h1>Loot at him!</h1>
+        <img src="/img/banner.jpg">
+    </body>
+</html>
